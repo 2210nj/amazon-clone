04c58539963a3fdcee371b2a485a8b04d614d749
Namita Jadhav <2210nj@gmail.com>
2020-10-31 14:46:00 +0530

    Implement authentication by email, password

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    //firebase login stuff
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/home">
@@ -15,18 +44,38 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name=""
+            id=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" name="" id="" />
+          <input
+            type="password"
+            name=""
+            id=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
-        <button className="login__registerButton">Create Account</button>
+        <button className="login__registerButton" onClick={register}>
+          Create Account
+        </button>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBqGlp_geZ_2QY6lkpBLbJsbUxEZul2A5g",
-    authDomain: "clone-eb9f7.firebaseapp.com",
-    databaseURL: "https://clone-eb9f7.firebaseio.com",
-    projectId: "clone-eb9f7",
-    storageBucket: "clone-eb9f7.appspot.com",
-    messagingSenderId: "251138361390",
-    appId: "1:251138361390:web:6f5fa76b5323f44199f7bf",
-    measurementId: "G-TG6XV82FRF"
-  };
+  apiKey: "AIzaSyBqGlp_geZ_2QY6lkpBLbJsbUxEZul2A5g",
+  authDomain: "clone-eb9f7.firebaseapp.com",
+  databaseURL: "https://clone-eb9f7.firebaseio.com",
+  projectId: "clone-eb9f7",
+  storageBucket: "clone-eb9f7.appspot.com",
+  messagingSenderId: "251138361390",
+  appId: "1:251138361390:web:6f5fa76b5323f44199f7bf",
+  measurementId: "G-TG6XV82FRF",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
