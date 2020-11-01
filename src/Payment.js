660869b34f0ea7ba4ddecdce1f197ff54b5b01d4
Namita Jadhav <2210nj@gmail.com>
2020-11-01 22:10:21 +0530

    setup stripe payment

--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getCartTotal } from "./reducer";
+import axios from "axios";
 
 function Payment() {
   const [{ cart, user }, dispatch] = useStateValue();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState(false);
+
+  const [clientSecret, setClientSecret] = useState(true);
+  const history = useHistory();
+  useEffect(() => {
+    //generate the special strip secret which allows us to charge the end user
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        //Strip expects total in  sub-currencies as in paisa/cents
+        url: `/payments/create?total=${getCartTotal(cart) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+    getClientSecret();
+  }, [cart]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        //PaymentIntent =  payment confirmation
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        history.replace("/orders");
+      });
+  };
+  const handleChange = (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  //   const payload = await stripe
   return (
     <div className="payment">
       <div className="payment__container">
@@ -40,7 +91,30 @@ function Payment() {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details">{/* Stripe magic */}</div>
+          <div className="payment__details">
+            {/* Stripe magic */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getCartTotal(cart)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"Rs. "}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Proceesing..</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
